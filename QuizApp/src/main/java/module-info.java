3dc7094cb57b5c405055eb4a2612d88b5beeac39
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2025-06-17 11:10:34 +0700

    create project

---
+++ QuizApp/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.dht.quizapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.base;
+    requires java.sql;
+    requires lombok;
+
+    opens com.dht.quizapp to javafx.fxml;
+    exports com.dht.quizapp;
+}
